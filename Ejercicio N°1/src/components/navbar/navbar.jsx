9d80b388f44ca0716980d8a0cbd176f3b89e753a
Fausto Desojo <faustodesojo@hotmail.com>
2023-11-17 11:23:25 -0300

    Add Context API to Hamburguer Menu

--- Ejercicio N°1/src/components/navbar/navbar.jsx
+++ Ejercicio N°1/src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { BiSolidUser } from "react-icons/bi";
 import { IoBed, IoClose } from "react-icons/io5";
 import {
@@ -14,14 +14,11 @@ import {
   NavImg
 } from "./styles";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { BurguerContext } from "../../context/hamburguer_menu";
 
 export const Navbar = () => {
-  const [click, setClick] = useState(false);
+  const { click, changeClick } = useContext(BurguerContext);
 
-  const ChangeClick = () => {
-    setClick(!click);
-  };
-  
   return (
     <NavbarContainer>
       <NavImg>
@@ -30,35 +27,35 @@ export const Navbar = () => {
           alt="logo-hotel"
         />
       </NavImg>
-        <HamburguerMenu onClick={ChangeClick}>
-          {click ? <IoClose /> : <RxHamburgerMenu />}
-        </HamburguerMenu>
+      <HamburguerMenu onClick={changeClick}>
+        {click ? <IoClose /> : <RxHamburgerMenu />}
+      </HamburguerMenu>
       <NavbarLink>
         <NavbarList click={click}>
-          <Links onClick={ChangeClick}>
+          <Links onClick={changeClick}>
             <Link>Inicio</Link>
           </Links>
-          <Links onClick={ChangeClick}>
+          <Links onClick={changeClick}>
             Habitaciones<Link></Link>
           </Links>
-          <Links onClick={ChangeClick}>
+          <Links onClick={changeClick}>
             Servicios<Link></Link>
           </Links>
-          <Links onClick={ChangeClick}>
+          <Links onClick={changeClick}>
             Galeria<Link></Link>
           </Links>
           <div>
-          <HomeContainerResponsive>
-          <button>
-            <BiSolidUser></BiSolidUser>
-            <span>Registrarse</span>
-          </button>
-          <button>
-            <IoBed></IoBed>
-            <span>Reservas</span>
-          </button>
-        </HomeContainerResponsive>
-            </div>
+            <HomeContainerResponsive>
+              <button>
+                <BiSolidUser></BiSolidUser>
+                <span>Registrarse</span>
+              </button>
+              <button>
+                <IoBed></IoBed>
+                <span>Reservas</span>
+              </button>
+            </HomeContainerResponsive>
+          </div>
         </NavbarList>
       </NavbarLink>
       <NavbarLinks>
--- Ejercicio N°1/src/screens/home/home_screen.jsx
+++ Ejercicio N°1/src/screens/home/home_screen.jsx
@@ -1,22 +1,25 @@
 // rafc **para  crear componentes automaticamente//extension**
-import React from 'react'
-import { Hero } from '../../components/hero/hero'
-import { Header } from '../../components/header/header'
-import { Navbar } from '../../components/navbar/navbar'
-import {HomeLayout} from '../../layout/home/home_layout'
-import { RoomsWidget } from '../../components/rooms/rooms_widget/rooms_widget'
-import { Footer } from '../../footer/footer'
+import React from "react";
+import { Hero } from "../../components/hero/hero";
+import { Header } from "../../components/header/header";
+import { Navbar } from "../../components/navbar/navbar";
+import { HomeLayout } from "../../layout/home/home_layout";
+import { RoomsWidget } from "../../components/rooms/rooms_widget/rooms_widget";
+import { Footer } from "../../footer/footer";
+import { MenuProvider } from "../../context/hamburguer_menu";
 
 export const HomeScreen = () => {
   return (
     <>
-      <Navbar />
+      <MenuProvider>
+        <Navbar />
+      </MenuProvider>
       <HomeLayout>
         <Header />
         <Hero />
-      <RoomsWidget />
+        <RoomsWidget />
       </HomeLayout>
       <Footer />
     </>
-  )
-}
+  );
+};
