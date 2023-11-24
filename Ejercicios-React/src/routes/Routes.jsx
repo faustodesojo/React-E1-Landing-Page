ade33486c469de75d54b1b0ab8d586f929627156
Fausto Desojo <faustodesojo@hotmail.com>
2023-11-24 14:03:43 -0300

    end e2

--- Ejercicios-React/src/routes/Routes.jsx
+++ Ejercicios-React/src/routes/Routes.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes as ReactDomRoutes } from "react-router-dom";
 import { Contact } from "../pages/contact/contact";
-import { About } from "../pages/about/About";
+import { About } from "../pages/about/about";
 import { HomeLayout } from "../layout/home/home_layout";
 import { Home } from "../pages/home/home";
 import { RoomsWidget } from "../pages/rooms/rooms_widget/rooms_widget";
