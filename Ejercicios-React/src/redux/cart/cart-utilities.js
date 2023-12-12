f7e750a851c8db433cab01caf1eeea81b04503bb
Fausto Desojo <faustodesojo@hotmail.com>
2023-12-12 14:05:19 -0300

    ads

--- Ejercicios-React/src/redux/cart/cart-utilities.js
+++ Ejercicios-React/src/redux/cart/cart-utilities.js
@@ -1,31 +1,38 @@
 export const addItemToCart = (cartItems, itemToAdd) => {
-    const existingItem = cartItems.find(
-        (cartItem) => cartItem.id === itemToAdd.id
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === itemToAdd.id
+  );
+
+  if (existingItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
+  }
 
-    if (existingItem) {
-        return cartItems.map((cartItem) =>
-            cartItem.id === itemToAdd.id
-                ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
-        );
-    }
-
-    return [...cartItems, { ...itemToAdd, quantity: 1 }];
-}
+  return [...cartItems, { ...itemToAdd, quantity: 1 }];
+};
 
 export const removeItemFromCart = (cartItems, itemToRemove) => {
-    const existingItem = cartItems.find(
-        (cartItem) => cartItem.id === itemToRemove.id
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === itemToRemove.id
+  );
+
+  if (existingItem.quantity > 1) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === itemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
     );
+  }
 
-    if (existingItem.quantity > 1) {
-        return cartItems.map((cartItem) =>
-            cartItem.id === itemToRemove.id
-                ? { ...cartItem, quantity: cartItem.quantity - 1 }
-                : cartItem
-        );
-    }   
+  return cartItems.filter((cartItem) => cartItem.id !== itemToRemove.id);
+};
 
-    return cartItems.filter((cartItem) => cartItem.id !== itemToRemove.id);
-}
+export const resetShippingCost = (cartItems, shippingCost) => {
+  if (cartItems.length === 1 && cartItems[0].quantity === 1) {
+    return 0;
+  }
+  return shippingCost;
+};
